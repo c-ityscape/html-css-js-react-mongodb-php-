16b8597d97f823ed5be8c5a4e6931b399af2b661
claris <114950582+c-ityscape@users.noreply.github.com>
2025-06-18 22:13:57 +0530

    18/06

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -4,21 +4,22 @@ import Button from './Button.jsx';
 import UserGreeting from './UserGreeting.jsx';
 import List from './List.jsx';
 function App() {
-  const fruits = [{name: 'Apple', calories: 95}, {name: 'Banana', calories: 105}, {name: 'Cherry', calories: 50}, {name: 'Date', calories: 277}, {name: 'Elderberry', calories: 73}];
-  const vegetables = [{name: 'Carrot', calories: 41}, {name: 'Broccoli', calories: 55}, {name: 'Spinach', calories: 23}, {name: 'Potato', calories: 77}, {name: 'Cucumber', calories: 16}];
+  // const fruits = [{name: 'Apple', calories: 95}, {name: 'Banana', calories: 105}, {name: 'Cherry', calories: 50}, {name: 'Date', calories: 277}, {name: 'Elderberry', calories: 73}];
+  // const vegetables = [{name: 'Carrot', calories: 41}, {name: 'Broccoli', calories: 55}, {name: 'Spinach', calories: 23}, {name: 'Potato', calories: 77}, {name: 'Cucumber', calories: 16}];
   return(
-<>
-<Card/>
+/*<>
+{ <Card/>
 !<Student name="John Doe" age={30} isStudent={true}/>
 <Student name="Patrick" age={42} isStudent={false}/>
-<Student></Student>
+<Student></Student> }
 <Button/>
-<UserGreeting isLoggedIn={true} username="BroCode"/>
+{/* <UserGreeting isLoggedIn={true} username="BroCode"/>
 {fruits.length>0? <List items={fruits} category="Fruits"/>: <p>No fruits available</p>}
 {vegetables.length>0? <List items={vegetables} category="Vegetables"/>: <p>No vegetables available</p>}
-{fruits.length>0 && <List items={fruits} category="Fruits"/>}
+{fruits.length>0 && <List items={fruits} category="Fruits"/>} }
 
-</>
+</>*/
+<Button />
 
 );
 }
--- react/my-react-app/src/Button.jsx
+++ react/my-react-app/src/Button.jsx
@@ -1,19 +1,32 @@
 
-function Button(){
-    const styles={
+// function Button(){
+//     const styles={
        
-            backgroundColor: 'blue',
-            color: 'white',
-            padding: '10px 20px',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer'
-        }
+//             backgroundColor: 'blue',
+//             color: 'white',
+//             padding: '10px 20px',
+//             border: 'none',
+//             borderRadius: '5px',
+//             cursor: 'pointer'
+//         }
     
+//     return (
+//         <>
+//         <button style={styles}>Click Me</button>
+//         </>
+//     );
+// }
+// export default Button
+function Button() {
+    const handleClick= () =>{
+        console.log("OUch! You clicked me!");
+    
+    }
+    const handleClick2 = (name) => console.log(`${name} stopc clicking me!`);
     return (
         <>
-        <button style={styles}>Click Me</button>
+        <button onClick={() => handleClick2("Bro")}>Click Me 😀</button>
         </>
-    );
+    )
 }
 export default Button;
